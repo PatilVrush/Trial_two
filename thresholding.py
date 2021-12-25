7df927a22ddbf602673de2f5d0c23ecf09903ec7
Vrushali Patil <soundarya.patil@gmail.com>
2021-12-24 22:40:04 -0500

    Added a print statement, changed threshold values, changed file name

--- thresholding.py
+++ thresholding.py
@@ -1,15 +1,15 @@
 import numpy as np
 import cv2
 
-img1 = cv2.imread('bookpage.jpg' , 1)
+img1 = cv2.imread('book.jpeg' , 1)
 
-ret,threshold = cv2.threshold(img1 , 12 , 255 , cv2.THRESH_BINARY) #low-light image, anything above 12 = white
+ret,threshold = cv2.threshold(img1 , 150 , 255 , cv2.THRESH_BINARY) #low-light image, anything above 12 = white
 
 img1_gray = cv2.cvtColor(img1 , cv2.COLOR_BGR2GRAY)
 
-ret2,threshold2 = cv2.threshold(img1_gray , 12 , 255 , cv2.THRESH_BINARY) #low-light image, anything above 12 = white
+ret2,threshold2 = cv2.threshold(img1_gray , 120 , 255 , cv2.THRESH_BINARY) #low-light image, anything above 12 = white
 
-
+print("Yo, in branch main")
 
 cv2.imshow('ORIGINAL' , img1)
 
